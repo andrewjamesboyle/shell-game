06d0218c1613526b3f2c4a60b84ae35c26d0a32b
ANDREW JAMES BOYLE <ANDREWBOYLEMUSIC@GMAIL.COM>
2022-07-26 20:11:26 -0700

    the game sort of works

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 // import functions and grab DOM elements
 const cupOneImg = document.getElementById('cup-one-image');
 const cupTwoImg = document.getElementById('cup-two-image');
-const cupThreeImg = document.getElementById('cup-three-img');
+const cupThreeImg = document.getElementById('cup-three-image');
 
 const cupOneButton = document.getElementById('cup-one-button');
 const cupTwoButton = document.getElementById('cup-two-button');
 const cupThreeButton = document.getElementById('cup-three-button');
+const resetButton = document.getElementById('play-again');
 
 const winsEl = document.getElementById('wins');
 const lossesEl = document.getElementById('losses');
 const totalEl = document.getElementById('total');
 
-const resetButton = document.getElementById('play-again');
-
-const hidingSpot = document.getElementsByClassName('hiding-spot');
+const ballLocation = ['cup one', 'cup two', 'cup three'];
 
 let wins = 0;
 let losses = 0;
@@ -31,12 +30,55 @@ function displayScore() {
   totalEl.textContent = total;
 }
 
+function disableButtons() {
+  cupOneButton.disabled = true;
+  cupTwoButton.disabled = true;
+  cupThreeButton.disabled = true;
+}
 
+function theGameFunction(correctGuess, userGuess) {
+  if (userGuess === correctGuess) {
+    console.log('You win!');
+    wins++;
+  } else {
+    console.log('You lose!');
+    losses++;
+  }}
 
+cupOneButton.addEventListener('click', () => {
+  console.log('user clicked cup one');
+  const hidingSpot = Math.floor(Math.random() * 3);
+  const answer = ballLocation[hidingSpot];
+  theGameFunction(answer, 'cup one');
+  total++;
+  displayScore();
+  disableButtons();
+});
 
-// let state
+cupTwoButton.addEventListener('click', () => {
+  console.log('user clicked cup two');
+  const hidingSpot = Math.floor(Math.random() * 3);
+  const answer = ballLocation[hidingSpot];
+  theGameFunction(answer, 'cup two');
+  total++;
+  displayScore();
+  disableButtons();
+});
 
-// set event listeners 
-  // get user input
-  // use user input to update state 
-  // update DOM to reflect the new state
+cupThreeButton.addEventListener('click', () => {
+  console.log('user click cup three');
+  const hidingSpot = Math.floor(Math.random() * 3);
+  const answer = ballLocation[hidingSpot];
+  theGameFunction(answer, 'cup three');
+  total++;
+  displayScore();
+  disableButtons();
+});
+
+resetButton.addEventListener('click', () => {
+  console.log('user clicked play again');
+  resetCups();
+  cupOneButton.disabled = false;
+  cupTwoButton.disabled = false;
+  cupThreeButton.disabled = false;
+});
